08f45648e37afcc68a7af52a2d2616479ab2a136
avanthik <avanthik33@gmail.com>
2024-11-25 13:31:22 +0530

    added testing code for notFounc component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import useProduct from "../hooks/useProduct";
 import Error from "../components/Error";
 import Products from "../components/Products";
